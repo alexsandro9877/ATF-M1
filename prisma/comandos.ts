1452222874c92cb1e7af6031f0cde1dfa49d8c2e
alex-Sandro-alves-de-lima <lexjustin5@gmail.com>
2025-02-03 10:38:53 -0300

    Inciando as primeiras configurações do projeto- 'inciando o projeto vercel'

--- prisma/comandos.ts
+++ prisma/comandos.ts
@@ -6,8 +6,17 @@
 // ----Criar as conexao com banco
 // ----Comando para criar tabela no banco
 //     npx prisma migrate dev --name init
+// npx prisma generate
 // ---Apos criar ver as tabelas no banco, executar o sofware DBeaver que ja instalei.
 // ---Entao conexao com o banco que criou Postgres
 
+
+
+
 // --- criar o seed
+// incluir no package.json
+// "prisma": {
+//     "seed": "tsx prisma/seed.ts"
+//   },
+//   se precisard  "build": "tsc -b && vite build",
 // --- npx prisma db seed
