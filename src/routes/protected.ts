940dff1f6059638697837156f40111f68cd960df
alex-Sandro-alves-de-lima <lexjustin5@gmail.com>
2025-06-10 19:27:50 -0300

    ssssssssssssssssssssssssssssss

--- src/routes/protected.ts
+++ src/routes/protected.ts
@@ -51,10 +51,6 @@ export async function protectedRoutes(app: FastifyInstance) {
         <head>
             <meta charset="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
-            />
             <title>Automatfull</title>
             <meta
             name="description"
