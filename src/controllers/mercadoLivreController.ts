a2090225d54aa12f4f7a3da7f0a1e79a6e40516d
alex-Sandro-alves-de-lima <lexjustin5@gmail.com>
2025-06-10 19:10:59 -0300

    xz\

--- src/controllers/mercadoLivreController.ts
+++ src/controllers/mercadoLivreController.ts
@@ -1,34 +1,45 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { responseToken, responseTokenCache } from "../services/mercadoLivreService";
+import {
+  responseToken,
+  responseTokenCache,
+} from "../services/mercadoLivreService";
 
- const mercadoLivreCallback = async (req, reply) => {
-  return "as";
+const mercadoLivreCallback = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  try {
+    reply.send("tokenReq");
+  } catch (error) {
+    //@ts-ignore
+    reply.status(500).send({ message: error.message });
+  }
 };
 
 class mercadolibreResToken {
-    async handle(request: FastifyRequest, reply: FastifyReply) {
-        const ResponseToken = new responseToken
-        try {
-            const tokenReq = await ResponseToken.execute();
-            reply.send(tokenReq);
-        } catch (error) {
-            //@ts-ignore
-            reply.status(500).send({ message: error.message });
-        }
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const ResponseToken = new responseToken();
+    try {
+      const tokenReq = await ResponseToken.execute();
+      reply.send(tokenReq);
+    } catch (error) {
+      //@ts-ignore
+      reply.status(500).send({ message: error.message });
     }
+  }
 }
 
 class mercadolibreResCache {
-    async handle(request: FastifyRequest, reply: FastifyReply) {
-        const ResponseTokenCache = new responseTokenCache
-        try {
-            const tokenReq = await ResponseTokenCache.execute();
-            reply.send(tokenReq);
-        } catch (error) {
-            //@ts-ignore
-            reply.status(500).send({ message: error.message });
-        }
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const ResponseTokenCache = new responseTokenCache();
+    try {
+      const tokenReq = await ResponseTokenCache.execute();
+      reply.send(tokenReq);
+    } catch (error) {
+      //@ts-ignore
+      reply.status(500).send({ message: error.message });
     }
+  }
 }
 
-export{mercadolibreResToken, mercadoLivreCallback,mercadolibreResCache}
+export { mercadolibreResToken, mercadoLivreCallback, mercadolibreResCache };
