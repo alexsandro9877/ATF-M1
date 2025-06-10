5e89155ad64a4d512dda33140c6be7b5d2415691
alex-Sandro-alves-de-lima <lexjustin5@gmail.com>
2025-06-10 19:23:36 -0300

    aaaaaaaaaaaaaaaa

--- dist/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import { protectedRoutes } from './protected';
-import { mercadoLivreRoutes } from './mercadoLivreRoutes';
-export async function routes(app) {
-    // Rota de proteção para gerar token
-    // app.register(authRoutes);
-    // Rotas protegidas
-    app.register(protectedRoutes);
-    app.register(mercadoLivreRoutes);
-}
